593468f5a9ab2de6b3c32980bc07b1f05cd8a6ad
Jaeden Waller <146506880+Fighterplane2@users.noreply.github.com>
2024-07-11 19:02:02 +0000

    MAJOR changes, moon, iss(incomplete), earth

--- main.js
+++ main.js
@@ -1,60 +1,60 @@
 let map;
-let theShader;
+let mapMoon;
+let blue;
 
 function preload() {
-  map = loadImage("./images/jupiter.png");
-  theShader = loadShader('shader.vert','shader.frag')
+  map = loadImage("./images/earth1.png");
+  mapMoon = loadImage("./images/moon.png");
+  blue = loadImage("./images/blue.png")
 }
 
 function setup() { 
   createCanvas(1200, 600, WEBGL);
 
-  // diagonal = dist(0,0,width,height);
-  
-  // centerX = width/2;
-  // centerY = height/2;
   noStroke();
 } 
 
 function draw() { 
-  shader(theShader);
-  rect(0,0,width,height);
-    // background(255,0,0);
-    // orbitControl();
-    // smooth();
-    // push();
-    // translate(0, 0);
-    // texture(map);
-    // ellipsoid();
-    // pop();
+
+  perspective(.5, 2, 100, 30000)
+
+   //This section is the Earth and the texture for the Earth
+    background(255,0,0);
+    orbitControl();
+    smooth();
+    push();
+    rotateWithFrameCount();
+    translate(0, 0, 0);
+    texture(map);
+    sphere(200);
+    pop();
+
+   //This section is the Moon and the texture for the Moon
+    push();
+    rotateWithFrameCountMoon();
+    translate(0, 0, 6000);
+    rotate(0);
+    texture(mapMoon);
+    sphere(50);
+    pop();
+
+    push();
+    rotateWithFrameCountBlue();
+    translate(0, 0, 207);
+    rotate(51.6);
+    texture(blue);
+    sphere(2);
+    pop();
 }
 
-// function draw(shadow){
-//     spacingX = width / 70;
-//     spacingY = width / 70;
+function rotateWithFrameCount(){
+  rotateY(frameCount/120);
+}
 
-//     for (x=0; x<=width; x += spacingX) {
-//         for (y=0; y<=height; y += spacingY) {
-          
-//           mouseToShape = dist(x, y, mouseX, mouseY);
-//           mouseToCenter = dist(centerX, centerY, mouseX, mouseY);
-          
-//           size = map(mouseToShape, 0, diagonal, 0, 400);
-  
-        
-//           wobble = 0;
-//           maxWobble = random(0,50);
-  
-//         fill(0,0,0,100);
-//         circle(x+wobble,y+wobble,size);
-//         }
-//     }
-// }
+function rotateWithFrameCountMoon(){
+  rotateY(frameCount/3360);
+}
 
-// function rotateRect(x, y, w, h, rot) {
-//     push()
-//     translate(x, y)
-//     rotate(rot)
-//     rect(0, 0, w, h)
-//     pop()
-//   }
+function rotateWithFrameCountBlue(){
+  rotateY(frameCount/70);
+}
